20092c49f1d7c7ed70c168bd1f71ee98a9ada916
BradenKeeler1 <bradenmkeeler@gmail.com>
2025-10-13 09:29:17 -0500

    Add files via upload

---
+++ CH1/me.js
@@ -0,0 +1,14 @@
+let name = "Braden";
+
+let favoriteColor = "Blue";
+
+let hobby = "Skating";
+
+let favoriteFood = "Steak";
+
+let favoriteAnimal = "Snakes";
+
+console.log("My name is " + Braden);
+console.log("My favorite color is " + Blue);
+console.log("My favorite hobby is " + Skating);
+console.log("My favorite food is " + Steak);
